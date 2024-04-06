aa542ddd91003318fd5339f5fb38d0f72c62cbe0
Evangelos Spyromilios <vsp84@icloud.com>
2024-04-06 22:10:40 +0200

    Initial Commit

---
+++ MyndVault/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MyndVault
+//
+//  Created by Evangelos Spyromilios on 06.04.24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
