3e5396b37fb1b3d7af3760a56eeba585449c0779
Evangelos Spyromilios <vsp84@icloud.com>
2025-06-06 16:06:06 +0200

    Folder cleanUp and Restruct. - init commit for Widget

---
+++ MnimiWidget/MnimiWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MnimiWidgetBundle.swift
+//  MnimiWidget
+//
+//  Created by Evangelos Spyromilios on 06.06.25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MnimiWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MnimiWidget()
+    }
+}
